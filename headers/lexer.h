a22d3036f72dcf1960a2b38a9244a5d00a008c43
Amey Agrawal <agrawalamey12@gmail.com>
2017-03-02 18:50:09 +0530

    Fixed lexer.h

--- headers/lexer.h
+++ headers/lexer.h
@@ -4,22 +4,21 @@
 
 int isSpace(char ch);
 
-
 int isNewLine(char ch);
 
 int isAlphabet(char ch);
 
 int isDigit(char ch);
 
-tokenInfo getToken(char * lex,int l,tokenInfo tok);
+Token createToken(char * lex,int l,Token tok);
 
 FILE* getStream(FILE *fp,char *buffer,int size);
 
-tokenInfo getNextToken(FILE* fp,char* buffer,int size);
+Token getNextToken(FILE* fp,char* buffer,int size);
 
 void removeComments(char *testcaseFile,char *cleanFile);
 
-tokenInfo* getTokens(const char* filename,tokenInfo* tokenArray);
+Token* getTokens(const char* filename,Token* tokenArray);
 
 
 #endif // LEXSER
